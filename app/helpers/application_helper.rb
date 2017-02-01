15964234447ecb15d65706a712612706922a6d2b
liyang <liyang@indeed.com>
2017-02-01 23:56:11 +0900

    Make sorting keep page number ;)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def sortable(column, title = nil, cust_css_class = nil)
+  def sortable(column, title = nil, page = nil, cust_css_class = nil)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, params.permit(:title, :participate_count, :created_at)
-      .merge(:sort => column, :direction => direction, :page => nil), {
+      .merge(:sort => column, :direction => direction, :page => page), {
         :class => [css_class, "glyphicon", "glyphicon-sort", cust_css_class]}
   end
 end
