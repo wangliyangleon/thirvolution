b835b8723e24c7509d4f87da240f60685d34b4e3
liyang <liyang@indeed.com>
2017-02-01 14:01:56 +0900

    Make the activity list nicer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
 module ApplicationHelper
-  def sortable(column, title = nil)
+  def sortable(column, title = nil, cust_css_class = nil)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, params.permit(:title, :participate_count, :created_at).merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}
+    link_to title, params.permit(:title, :participate_count, :created_at)
+      .merge(:sort => column, :direction => direction, :page => nil), {
+        :class => [css_class, "glyphicon", "glyphicon-sort", cust_css_class]}
   end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  def activity_time_format(date_time)
+    if date_time > Time.zone.now.beginning_of_day()
+      date_time.strftime("%H:%M")
+    else
+      date_time.strftime("%-m-%-d")
+    end
+  end
 end
